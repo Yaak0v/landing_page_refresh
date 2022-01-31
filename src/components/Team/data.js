4335a7ea9807faa29587f9247e8c16805bd23876
Yaak0v <jacobsandry@gmail.com>
2022-01-31 11:16:03 -0500

    add carlos and darshak

--- src/components/Team/data.js
+++ src/components/Team/data.js
@@ -5,6 +5,8 @@ export const TeamData ={
         {Name: "Will Creel", Title: "Senior Operations Manager", Image: require("../../images/headshots/will.jpg").default, LinkedIn:"https://www.linkedin.com/in/wcreel/"},
         {Name: "Jennifer Guay", Title: "Product Operations Manager", Image: require("../../images/headshots/jenn.jpg").default, LinkedIn:"https://www.linkedin.com/in/jennifer-guay-707336123/"},
         {Name: "Joseph Harrington", Title: "Project Manager", Image: require("../../images/headshots/joe.jpg").default, LinkedIn:"https://www.linkedin.com/in/joe-harrington-pe-a9929521/"},
-        {Name: "Tamara Jackson", Title: "Project Analyst", Image: require("../../images/headshots/tamara.jpg").default, LinkedIn:"https://www.linkedin.com/in/tamara-jackson-1784a4bb/"}
+        {Name: "Tamara Jackson", Title: "Project Analyst", Image: require("../../images/headshots/tamara.jpg").default, LinkedIn:"https://www.linkedin.com/in/tamara-jackson-1784a4bb/"},
+        {Name: "Carlos Molina", Title: "Senior Software Engineer", Image: require("../../images/headshots/carlos.jpg").default, LinkedIn:"https://www.linkedin.com/in/carlos-molina-avenda%C3%B1o//"},
+        {Name: "Darshak Patel", Title: "Strategy and Ops Associate", Image: require("../../images/headshots/darshak.jpg").default, LinkedIn:"https://www.linkedin.com/in/darshakp/"}
     ]
 }
